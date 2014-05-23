2be96202f14e45f86a30454608d1b49d89436cc1
Lawrence Chang <lc.uclakers@gmail.com>
2014-05-23 13:47:37 -0400

    set up for heroku

--- server.rb
+++ server.rb
@@ -2,6 +2,37 @@ require 'sinatra'
 require 'shotgun'
 require 'CSV'
 require 'pry'
+require 'redis'
+require 'json'
+
+
+def get_connection
+  if ENV.has_key?("REDISCLOUD_URL")
+    Redis.new(url: ENV["REDISCLOUD_URL"])
+  else
+    Redis.new
+  end
+end
+
+def find_articles
+  redis = get_connection
+  serialized_articles = redis.lrange("slacker:articles", 0, -1)
+
+  articles = []
+
+  serialized_articles.each do |article|
+    articles << JSON.parse(article, symbolize_names: true)
+  end
+
+  articles
+end
+
+def save_article(url, title, description)
+  article = { url: url, title: title, description: description }
+
+  redis = get_connection
+  redis.rpush("slacker:articles", article.to_json)
+end
 
 def load_data(csv)
   master_data = []
